51acb578dd3028d1b1b4f0affe09e44a7ff7969f
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-04-23 18:44:32 -0300

    Soma Simples

--- AreaDoCirculo/Program.cs
+++ AreaDoCirculo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 //A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
 //- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
@@ -9,10 +10,22 @@
 //Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 
 
-namespace AreaDoCirculo{
+namespace AreaDoCirculo
+{
 
-    class Program{
-        static void Main(string[] args){
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A; //create vars
+            double pi = 3.14159; //create and declare var pi
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //read console and 
+
+            A = pi * (R * R);
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+
 
         }
     }
---
+++ SomaSimples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//Entrada
+//O arquivo de entrada contém 2 valores inteiros.
+//Saída
+//imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade 
+//seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
+//caso contrário, você receberá "Presentation Error".
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double A, B, S;
+
+        A = double.Parse(Console.ReadLine());//converte de string para double
+        B = double.Parse(Console.ReadLine());
+
+        S = A + B;
+        Console.WriteLine("SOMA = " + S.ToString());
+    }
+
+}
