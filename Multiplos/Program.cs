55488bdbb53f388c74d3c8c5900296f2cd346415
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-25 12:51:48 -0300

    Concluído

---
+++ Multiplos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] vetor = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(vetor[0]);
+            int B = int.Parse(vetor[1]);
+
+            if (A > B)
+            {
+                int resto = A % B;
+                if (resto == 0)
+                {
+                    System.Console.WriteLine("Sao Multiplos");
+                }
+                else
+                {
+                    System.Console.WriteLine("Nao sao Multiplos");
+                }
+
+            }
+            else
+            {
+                int resto = B % A;
+                if (resto == 0)
+                {
+                    System.Console.WriteLine("Sao Multiplos");
+                }
+                else
+                {
+                    System.Console.WriteLine("Nao sao Multiplos");
+                }
+            }
+        }
+    }
+}
