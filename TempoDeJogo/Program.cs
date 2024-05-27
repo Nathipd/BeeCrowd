c6be14b633257daa251f20f82dc562af495a3c1e
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-26 21:28:48 -0300

    Concluído

---
+++ TempoDeJogo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TempoDeJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] VetorInput = Console.ReadLine().Split(' ');
+
+            int horaInicio = int.Parse(VetorInput[0]);
+            int horaFim = int.Parse(VetorInput[1]);
+
+            int resultado = 0;
+            if (horaFim < horaInicio)
+            {
+                resultado = 24 - horaInicio;
+                resultado = resultado + horaFim;
+            }
+            else if(horaInicio == horaFim){
+                resultado = 24;
+            }
+            else
+            {
+                resultado = horaFim - horaInicio;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {resultado} HORA(S)" );
+        }
+    }
+}
