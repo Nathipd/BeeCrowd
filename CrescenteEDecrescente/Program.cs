c7d2cd735426fe675d726e03e17bc93cb116bb05
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-29 16:33:29 -0300

    Concluído

---
+++ CrescenteEDecrescente/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace CrescenteEDecrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String[] vetorInput = Console.ReadLine().Split(' ');
+
+                int X = int.Parse(vetorInput[0]);
+                int Y = int.Parse(vetorInput[1]);
+                
+                if (X == Y)
+                {
+                    break;
+                }
+                if (X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else
+                {
+                    System.Console.WriteLine("Crescente");
+                }
+
+
+            }
+        }
+    }
+}
