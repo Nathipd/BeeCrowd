eb0cf3b34f4797281bf86a1301e711f7c9343030
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-29 19:51:08 -0300

    Concluído

---
+++ SenhaFixa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SenhaFixa{
+    class Program{
+        static void Main (string[] args){
+            while(true){
+                string[] vetorInput = Console.ReadLine().Split (' ');
+
+                int senha = int.Parse(vetorInput[0]);
+
+                if(senha == 2002){
+                    System.Console.WriteLine("Acesso Permitido");
+                    break;
+                }else{
+                    System.Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
