6f3277abdd1d922aa3c47b7d88e5016f92355e51
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-30 23:30:08 -0300

    Concluído

---
+++ Idades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Idades{
+    class Program{
+        static void Main(string[] args){
+            int soma = 0;
+            int amostras = 0;
+
+            while(true){
+                int idades = int.Parse(Console.ReadLine());
+    
+                if(idades < 0){
+                    break;
+                }
+                
+                amostras ++;
+
+                soma += idades;
+
+            }
+            float mediaIdades = (float)soma/(float)amostras;
+            System.Console.WriteLine(mediaIdades.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
