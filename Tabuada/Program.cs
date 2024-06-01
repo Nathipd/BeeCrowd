07058a7444d9eab393ede1cf909e98850e1da5e0
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-05-31 21:56:55 -0300

    Concluído

---
+++ Tabuada/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tabuada{
+    class Program{
+        static void Main(string[] args){
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i=1; i <= 10; i++){
+                System.Console.WriteLine($"{i} x {n} = " + (i*n));
+                
+            }
+        }
+    }
+}
