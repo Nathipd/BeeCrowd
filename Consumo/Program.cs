f697899eee2ae36ed25125183d02d6100affa0cd
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-07-09 19:01:15 -0300

    Exercício concluído
    Signed-off-by: Nathalia Dias <nathaliapiresdias0@gmail.com>

---
+++ Consumo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Qual a distância percorrida
+            int distancia = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            //Qual o gasto de combustível
+            double combustivel = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            ConsumoMedio(distancia, combustivel);
+        }
+        static void ConsumoMedio(int distancia, double combustivel)
+        {
+            double consumoMedio = distancia / combustivel;
+
+            System.Console.WriteLine($"{consumoMedio.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+        }
+    }
+}
